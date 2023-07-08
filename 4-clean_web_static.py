e45a64823eaacf1706f3c02cbc606c9b8a311d45
Michael Peter <michaelhpet@gmail.com>
2023-07-09 00:11:37 +0100

    feat: cleaup archives

--- 4-clean_web_static.py
+++ 4-clean_web_static.py
@@ -53,3 +53,25 @@ def deploy():
     """Archive and deploy new version."""
     archive_path = do_pack()
     return do_deploy(archive_path) if archive_path else False
+
+
+def do_clean(number=0):
+    """Delete outdated archives."""
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
