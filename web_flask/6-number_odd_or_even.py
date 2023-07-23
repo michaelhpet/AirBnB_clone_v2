e401b2cd9cb8a7f7a27b995a82e0dc32816370b3
Michael Peter <michaelhpet@gmail.com>
2023-07-23 21:19:40 +0100

    feat: render templates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,5 +45,11 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Render number odd or even template."""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
