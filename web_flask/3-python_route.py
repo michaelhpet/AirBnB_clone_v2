7cb9d5c1aa4270e92fc29a2ed59def86286335b7
Michael Peter <michaelhpet@gmail.com>
2023-07-23 20:55:08 +0100

    fix: add python route w/o params

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c(text):
     return f"C {text}"
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """Handle Python route."""
